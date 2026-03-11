07f3f41cc4531f40e64accba6ffe412ceaafbcac
enzobalderrama <76264578+enzobalderrama@users.noreply.github.com>
2026-03-11 01:05:31 -0600

    Nearly done (again)

--- src/Sort.java
+++ src/Sort.java
@@ -13,31 +13,70 @@ public class Sort {
         }
 
 
-        ArrayList<String> sortedList = new ArrayList<>();
+        ArrayList<String> sortedList = unsortedList;
         int letterNum;
-        for (int i = 0; i < unsortedList.size(); i++) {
-            for (int j = 0; j < unsortedList.size(); j++) {
-                String extraHassle = unsortedList.get(i);
-                letterNum = Sort.toNum(extraHassle.substring(j,j+1).toLowerCase());
-                if (sortedList.isEmpty()) {
-                    sortedList.add(extraHassle);
-                }
-                else {
-                    if (letterNum < Sort.toNum(sortedList.get(j).substring(j,j+1))) {
-                        sortedList.add(j, extraHassle);
-                    }
-                    else {
-                        sortedList.add(j-1, extraHassle);
-                    }
-                }
+        for (int i = unsortedList.size(); i >= 0; i--) {
+            ArrayList<ArrayList<String>> letterCats = new ArrayList<>(27);
+            for (int j = 0; j < 27; j++) {
+                letterCats.add(new ArrayList<String>());
             }
+            for
         }
+
+
+
+
+        File newFile = new File("some_file_key.txt");
+        writeFile(outputName, sortedResult);
     }
 
-
+//String extraHassle = unsortedList.get(i);
+//                letterNum = Sort.toNum(extraHassle.substring(j,j+1).toLowerCase());
+//                if (sortedList.isEmpty()) {
+//                    sortedList.add(extraHassle);
+//                }
+//                else {
+//                    if (letterNum < Sort.toNum(sortedList.get(j).substring(j,j+1))) {
+//                        sortedList.add(j, extraHassle);
+//                    }
+//                    else {
+//                        sortedList.add(j-1, extraHassle);
+//                    }
+//                }
+    //// This is the core Radix Sort logic using your toNum concept
+    //    public static ArrayList<String> radixSort(ArrayList<String> list, int maxLen) {
+    //        ArrayList<String> currentList = list;
+    //
+    //        // We go from the LAST character to the FIRST (LSD Radix Sort)
+    //        for (int d = maxLen - 1; d >= 0; d--) {
+    //            // 1. Create 27 buckets (0 for 'end of word', 1-26 for a-z)
+    //            ArrayList<ArrayList<String>> buckets = new ArrayList<>(27);
+    //            for (int i = 0; i < 27; i++) buckets.add(new ArrayList<>());
+    //
+    //            // 2. Put each word into a bucket based on the character at position d
+    //            for (String word : currentList) {
+    //                int charIdx;
+    //                if (d < word.length()) {
+    //                    // Use your toNum logic on the specific character
+    //                    charIdx = toNum(word.substring(d, d + 1));
+    //                } else {
+    //                    charIdx = 0; // Word is shorter than d, goes to the "empty" bucket
+    //                }
+    //                buckets.get(charIdx).add(word);
+    //            }
+    //
+    //            // 3. Collect words back from buckets into the list
+    //            ArrayList<String> nextPass = new ArrayList<>();
+    //            for (ArrayList<String> bucket : buckets) {
+    //                nextPass.addAll(bucket);
+    //            }
+    //            currentList = nextPass;
+    //        }
+    //        return currentList;
+    //    }
 
     public static int toNum(String s){
-        s.toLowerCase();
+        s = s.toLowerCase();
         return switch (s) {
             case "a" -> 1;
             case "b" -> 2;
